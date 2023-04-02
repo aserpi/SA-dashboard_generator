dd1708687ff06d1193d58ad6fe115f773f72d1dd
Alessandro Serpi <alessandro.serpi@me.com>
2023-04-02 13:19:55 +0200

    feat: permissions templates

--- package/bin/sa_dashboard_generator/modalert_generate_dashboards_helper.py
+++ package/bin/sa_dashboard_generator/modalert_generate_dashboards_helper.py
@@ -3,6 +3,7 @@ import re
 import sys
 
 import import_declare_test  # Always put this line before third-party imports
+from solnlib.acl import ACLManager
 from solnlib.conf_manager import ConfManager, ConfManagerException, ConfStanzaNotExistException
 from solnlib.modular_input.checkpointer import KVStoreCheckpointer
 from solnlib.splunk_rest_client import SplunkRestClient
@@ -46,6 +47,25 @@ def _multiple_replace(pattern, repls, item):
     return pattern.sub(lambda m: repls[re.escape(m.group(0))], item)
 
 
+def _permissions_template(helper, template_name):
+    conf_manager = ConfManager(helper.session_key, helper.ta_name)
+    try:
+        templates = conf_manager.get_conf("sa_dashboard_generator_permissions_template")
+        template_params = templates.get(template_name)
+    except (KeyError, ConfManagerException, ConfStanzaNotExistException):
+        helper.log_error("Permissions template not found in configuration file.")
+        sys.exit(13)
+
+    owner = template_params.get("owner_")
+    perms_read = template_params.get("perms_read")
+    perms_write = template_params.get("perms_write")
+
+    perms_read = [perms_read] if isinstance(perms_read, str) else perms_read
+    perms_write = [perms_write] if isinstance(perms_write, str) else perms_write
+    template_params = {"owner": owner, "perms_read": perms_read, "perms_write": perms_write}
+    return {k: v for k, v in template_params.items() if v is not None}
+
+
 def _scheduled_view_template(helper, template_name):
     conf_manager = ConfManager(helper.session_key, helper.ta_name)
     try:
@@ -74,17 +94,21 @@ def process_event(helper, *args, **kwargs):
     del_prev = is_true(_get_param(helper, "del_prev"))
     del_regex = _get_param(helper, "del_regex")
     dest_app = _get_param(helper, "dest_app")
+    permissions_template = _get_param(helper, "permissions_template")
     template_dashboard_id = _get_param(helper, "template_dashboard_id")
     scheduled_view_template = _get_param(helper, "scheduled_view_template")
     src_app = _get_param(helper, "src_app")
 
     dest_app = dest_app if dest_app else src_app
+    acl_manager = ACLManager(helper.session_key, dest_app) if permissions_template else None
     dest_client = SplunkRestClient(helper.session_key, dest_app)
     src_client = SplunkRestClient(helper.session_key, src_app)
 
     template_dashboard_res = src_client.get(f"data/ui/views/{template_dashboard_id}", count=1,
                                             output_mode="json").body.read()
     template_dashboard_def = json.loads(template_dashboard_res)["entry"][0]["content"]["eai:data"]
+    template_permissions_params = (_permissions_template(helper, permissions_template)
+                                   if permissions_template else None)
     template_scheduled_view_params = (_scheduled_view_template(helper, scheduled_view_template)
                                       if scheduled_view_template else None)
 
@@ -121,6 +145,16 @@ def process_event(helper, *args, **kwargs):
         dashboard_ids.append(dashboard_id)
         helper.log_info(f"Created dashboard '{dashboard_id}'.")
 
+        permissions_params = {}
+        if acl_manager:
+            permissions_params = {k: _multiple_replace(pattern, repls, v)
+                                  for k, v in template_permissions_params.items()}
+            try:
+                acl_manager.update(f"{dashboard_url}/acl", **permissions_params)
+            except Exception:
+                helper.log_error(f"Error when changing permissions of dashboard '{dashboard_id}.")
+                continue
+
         if template_scheduled_view_params:
             scheduled_view_id = f"_ScheduledView__{dashboard_id}"
             scheduled_view_params = {k: _multiple_replace(pattern, repls, v)
@@ -133,6 +167,13 @@ def process_event(helper, *args, **kwargs):
             except Exception:
                 helper.log_error(f"Error when creating scheduled view '{scheduled_view_id}'.")
                 continue
+
+            if acl_manager:
+                try:
+                    acl_manager.update(f"{scheduled_view_url}/acl", **permissions_params)
+                except Exception:
+                    helper.log_error(f"Error when changing permissions of scheduled view "
+                                     f"'{scheduled_view_id}.")
 
     if helper.search_name:
         checkpoint.update(helper.search_name, {"dashboard_ids": dashboard_ids})
