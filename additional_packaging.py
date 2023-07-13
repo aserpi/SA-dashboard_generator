eac3d610e1efe0d5589fe9926d5a681eefa408e7
Alessandro Serpi <alessandro.serpi@me.com>
2023-07-13 10:40:38 +0200

    chore: remove executables

--- additional_packaging.py
+++ additional_packaging.py
@@ -9,5 +9,7 @@ def additional_packaging(ta_name):
     shutil.copy(Path("LICENSE.md"), output_path)
     shutil.copy(Path("README.md"), output_path)
 
+    # Avoid binaries without source code (Splunk Cloud check)
     subprocess.check_call([sys.executable, "-m", "pip", "install", "charset-normalizer",
                            "--no-binary=:all:", f"--target={output_path / 'lib'}", "--upgrade"])
+    shutil.rmtree(output_path / "lib/bin", ignore_errors=True)
