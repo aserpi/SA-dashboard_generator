78b321e8d27a37eb172458086664f889c1204ab2
Alessandro Serpi <alessandro.serpi@me.com>
2024-12-11 19:27:45 +0100

    fix: wrong parameters in scheduling template

--- package/bin/sa_dashboard_generator/_templates.py
+++ package/bin/sa_dashboard_generator/_templates.py
@@ -5,8 +5,9 @@ import import_declare_test  # Always put this line before third-party imports
 from solnlib.conf_manager import ConfManager, ConfManagerException, ConfStanzaNotExistException
 
 
-_scheduled_view_keys = ["action.email.include.view_link", "action.email.inline", "action.email.message",
-                        "action.email.sendpng", "action.email.sendpdf", "action.email.subject"]
+_scheduled_view_keys = ["action.email.include.view_link", "action.email.inline",
+                        "action.email.message.view", "action.email.sendpdf", "action.email.sendpng",
+                        "action.email.subject.view", "action.email.to"]
 
 def _permissions_template(helper, template_name):
     conf_manager = ConfManager(helper.session_key, helper.ta_name)
@@ -36,23 +37,27 @@ def _scheduled_view_template(helper, template_name):
         helper.log_error("Scheduled view template not found in configuration file.")
         sys.exit(12)
 
-    params = {"action.email.include.view_link": template_params["view_link"],
-              "action.email.inline": template_params["inline_png"],
-              "action.email.message": template_params["message"],
-              "action.email.sendpdf": template_params["send_pdf"],
-              "action.email.sendpng": template_params["send_png"],
-              "action.email.subject": template_params["subject"],
+    params = {"action.email.include.view_link": template_params.get("view_link"),
+              "action.email.inline": template_params.get("inline_png"),
+              "action.email.message.view": template_params.get("message"),
+              "action.email.sendpdf": template_params.get("send_pdf"),
+              "action.email.sendpng": template_params.get("send_png"),
+              "action.email.subject.view": template_params.get("subject"),
               "action.email.to": template_params["to"],
               "cron_schedule": template_params["cron_schedule"],
               "description": template_params.get("description")}
+    params = {k: v for k, v in params.items() if v is not None}
 
-    for param in re.split(r"(?<!\\)\n", template_params.get("email_params", "")):
-        if not param:
-            continue
-        key, value = param.split("=", 1)
-        key = key.strip()
-        if key in _scheduled_view_keys:
-            helper.log_info(f"Found '{key}' in 'action.email*' parameters, overriding the template field.")
-        params[key] = value.lstrip().replace("\\\n", "\n")
+    email_params = template_params.get("email_params")
+    if email_params is not None:
+        for param in re.split(r"(?<!\\)\n", email_params):
+            if not param:
+                continue
+            key, value = param.split("=", 1)
+            key = key.strip()
+            if key in _scheduled_view_keys:
+                helper.log_info(f"Found '{key}' in 'action.email*' parameters, overriding the "
+                                "template field.")
+            params[key] = value.lstrip().replace("\\\n", "\n")
 
     return params
