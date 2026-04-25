748f2261447f186e98cc4a4c58a0d02286e3fc2e
Alessandro Serpi <alessandro.serpi@me.com>
2026-04-25 17:33:14 +0200

    ci: move to arm runner

--- additional_packaging.py
+++ additional_packaging.py
@@ -1,6 +1,4 @@
 import shutil
-import subprocess
-import sys
 from pathlib import Path
 
 
@@ -8,8 +6,3 @@ def additional_packaging(ta_name):
     output_path = Path("output") / ta_name
     shutil.copy(Path("LICENSE.md"), output_path)
     shutil.copy(Path("README.md"), output_path)
-
-    # Avoid binaries without source code (Splunk Cloud check)
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "charset-normalizer",
-                           "--no-binary=:all:", f"--target={output_path / 'lib'}", "--upgrade"])
-    shutil.rmtree(output_path / "lib/bin", ignore_errors=True)
